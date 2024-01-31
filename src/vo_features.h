7f37f7052c2f71bfe22eb58f79114ef153b4aeeb
lan <mai.lauren2@gmail.com>
2024-01-30 19:20:54 -0600

    changed includes for opencv2 4.9

--- src/vo_features.h
+++ src/vo_features.h
@@ -28,12 +28,12 @@ THE SOFTWARE.
 #include "opencv2/opencv.hpp"
 #include "opencv2/xfeatures2d.hpp"
 
-#include "opencv2/video/tracking.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
+// #include "opencv2/video/tracking.hpp"
+// #include "opencv2/imgproc/imgproc.hpp"
+// #include "opencv2/highgui/highgui.hpp"
+// #include "opencv2/features2d/features2d.hpp"
+// #include "opencv2/calib3d/calib3d.hpp"
+// #include "opencv2/calib3d/calib3d.hpp"
 
 
 
